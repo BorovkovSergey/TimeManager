c2523860b7405de49719c48609d62c4f961c2764
Borovkov Sergey <707grays@gmail.com>
2020-10-29 21:09:29 +0700

    added TimeManagerCore

---
+++ TimeManagerCore/TimeManagerCore.swift
@@ -0,0 +1,14 @@
+//
+//  TimeManagerCore.swift
+//  TimeManagerCore
+//
+//  Created by Sergey Borovkov on 29.10.2020.
+//  Copyright © 2020 Sergey Borovkov. All rights reserved.
+//
+
+class TimeManagerCore {
+    public init()
+    {
+        
+    }
+}

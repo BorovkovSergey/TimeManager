896bda62dd4c614fdfc39a8855a2892398bf9953
Borovkov Sergey <707grays@gmail.com>
2020-10-29 16:20:50 +0700

    added UICollectionView on view controller

--- TimeMannager/ViewController.swift
+++ TimeMannager/ViewController.swift
@@ -7,35 +7,91 @@
 //
 
 import UIKit
-
 //class UIProgressBar: UIVi
-class ViewController: UIViewController {
-    
-    private lazy var textField : MyProgressView = {
-        let pv = MyProgressView(_height: 40, _labelText: "hello world", _maxValue: 60)
-        pv.translatesAutoresizingMaskIntoConstraints = false
-        pv.backgroundColor = UIColor.gray
-        //           pv.barHeight=height;
-        //           pv.progress = 0.0
-        pv.reachedValue=30
-        return pv
+class ViewController: UIViewController , UICollectionViewDataSource {
+
+    let theCollectionView: UICollectionView = {
+        let v = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout())
+        v.translatesAutoresizingMaskIntoConstraints = false
+        v.backgroundColor = .white
+        v.contentInsetAdjustmentBehavior = .always // ???
+        return v
+    }()
+
+    let rowsLayout : UICollectionViewFlowLayout = {
+        let v = UICollectionViewFlowLayout()
+        v.minimumLineSpacing = 0
+        return v
     }()
     
-    private func addSubviews(){
-        view.addSubview(textField)
-    }
-    
-    
+    // track collection view frame change
+    var colViewWidth: CGFloat = 0.0
+
+    // example data --- this will be filled with simple number strings
+    var theData: [String] = [String]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        addSubviews()
-        
+
+        view.backgroundColor = .systemYellow
+        view.addSubview(theCollectionView)
         NSLayoutConstraint.activate([
-            textField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0.0),
-            textField.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 0.0),
-            textField.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: 0.0),
+            theCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            theCollectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            theCollectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            theCollectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
         ])
-        // Do any additional setup after loading the view.
+
+        // register the two cell classes for reuse
+        theCollectionView.register(MyProgressView.self, forCellWithReuseIdentifier: "MyProgressView")
+        theCollectionView.register(UICollectionViewAddItemButton.self, forCellWithReuseIdentifier: "UICollectionViewAddItemButton")
+
+        // set collection view dataSource
+        theCollectionView.dataSource = self
+
+        // use custom flow layout
+        theCollectionView.collectionViewLayout = rowsLayout
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        rowsLayout.itemSize = CGSize(width: theCollectionView.frame.width, height: 40) // fixme 40
+    }
+//
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // return 1 more than our data array (the extra one will be the "add item" cell
+        return theData.count + 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        // if item is less that data count, return a "Content" cell
+        if indexPath.item < theData.count {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyProgressView", for: indexPath) as! MyProgressView
+            return cell
+        }
+
+        // past the end of the data count, so return an "Add Item" cell
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: "UICollectionViewAddItemButton", for: indexPath
+            ) as! UICollectionViewAddItemButton
+
+        // set the closure
+        cell.tapCallback = {
+            // add item button was tapped, so append an item to the data array
+            self.theData.append("\(self.theData.count + 1)")
+            // reload the collection view
+            collectionView.reloadData()
+            collectionView.performBatchUpdates(nil, completion: {
+                (result) in
+                // scroll to make newly added row visible (if needed)
+                let i = collectionView.numberOfItems(inSection: 0) - 1
+                let idx = IndexPath(item: i, section: 0)
+                collectionView.scrollToItem(at: idx, at: .bottom, animated: true)
+            })
+        }
+
+        return cell
+
     }
 }
-
